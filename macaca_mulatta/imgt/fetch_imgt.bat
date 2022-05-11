b02029d6178fbda9e9acc50598c1888a1b253742
William Lees <william@lees.org.uk>
2022-05-11 09:37:14 +0100

    Incorporate Martin Corcoran's powerpoint notes Add 'old' ad 'new' IMGT names Add kimdb genbank accessions and note where sanger validated Include all rhgldb heavy-chain sequences

--- macaca_mulatta/imgt/fetch_imgt.bat
+++ macaca_mulatta/imgt/fetch_imgt.bat
@@ -1,5 +1,9 @@
-wget http://www.imgt.org/download/GENE-DB/IMGTGENEDB-ReferenceSequences.fasta-nt-WithGaps-F+ORF+inframeP
+wget --no-check-certificate http://www.imgt.org/download/GENE-DB/IMGTGENEDB-ReferenceSequences.fasta-nt-WithGaps-F+ORF+inframeP
 python ../../python/extract_refs.py IMGTGENEDB-ReferenceSequences.fasta-nt-WithGaps-F+ORF+inframeP "Macaca mulatta"
-cat Macaca_mulatta_IGHV.fasta Macaca_mulatta_IGHD.fasta Macaca_mulatta_IGHJ.fasta >Macaca_mulatta_IGH.fasta
+cat Macaca_mulatta_IGHV.fasta Macaca_mulatta_IGHD.fasta Macaca_mulatta_IGHJ.fasta >Macaca_mulatta_IGH_29_apr_2022.fasta
 cd ../imgt_feb_2021
 python ../../python/extract_refs.py imgt_ref_with_gaps_18_feb_2021.fasta "Macaca mulatta"
+cat Macaca_mulatta_IGHV.fasta Macaca_mulatta_IGHD.fasta Macaca_mulatta_IGHJ.fasta >Macaca_mulatta_IGH_18_feb_2021.fasta
+cd ../imgt_may_2020
+python ../../python/extract_refs.py imgt_ref_2020_05_10.fasta "Macaca mulatta"
+cat Macaca_mulatta_IGHV.fasta Macaca_mulatta_IGHD.fasta Macaca_mulatta_IGHJ.fasta >Macaca_mulatta_IGH_10_may_2020.fasta
--- python/consolidate_rhesus.py
+++ python/consolidate_rhesus.py
@@ -1,7 +1,7 @@
 # Cross-reference allocated labels with the sources to provide a consolidated report
 
 import csv
-import simple_bio_seq as simple
+import receptor_utils.simple_bio_seq as simple
 import number_ighv
 
 
@@ -11,7 +11,7 @@ rhgldb = {}
 with open('../cottrell_et_al/gld_rev4_20211111.csv', 'r') as fi:
     reader = csv.DictReader(fi)
     for row in reader:
-        if len(row) > 1 and 'H' in row['gene_type'] and len(row['other_species']) < 1:
+        if len(row) > 1 and 'H' in row['gene_type']:
             if row['gene_sequence'] not in rhgldb:
                 rhgldb[row['gene_sequence']] = []
             if 'DH' in row['gene_type']:
@@ -46,41 +46,107 @@ for name, seq in genes.items():
         kimdb[row['Gene Sequence']] = []
     kimdb[row['Gene Sequence']].append(row)
 
-# these two are only used for gapping sequences
+# IMGT major updates: https://www.imgt.org/IMGTgenedbdoc/dataupdates.html#assemblies
+
+# these two are only used for gapping sequences. The gap change was introduced some time after this set was collected on 18 feb 2021
 imgt_v_gapped = simple.read_fasta('../imgt_feb_2021/Macaca_mulatta_IGHV_gapped.fasta')
 imgt_v_ungapped = simple.read_fasta('../imgt_feb_2021/Macaca_mulatta_IGHV.fasta')
 
-imgt = {}
-genes = simple.read_fasta('../imgt/Macaca_mulatta_IGH.fasta')
-for name, seq in genes.items():
-    row = {}
-    row['Gene Name'] = name
-    if 'IGHD' in name:
-        row['Type'] = 'IGHD'
-    elif 'IGHJ' in name:
-        row['Type'] = 'IGHJ'
-    elif 'IGHV' in name:
-        row['Type'] = 'IGHV'
+
+# The mass change of IMGT IGHV names was announced on 4 September 2020
+
+imgt_files = {
+    'post Sept 2021': '../imgt/Macaca_mulatta_IGH_29_apr_2022.fasta',
+    'pre Sept 2021': '../imgt_may_2020/Macaca_mulatta_IGH_10_may_2020.fasta',
+}
+
+kimdb_accessions = {}
+kimdb_accessions['inferred'] = {}
+kimdb_accessions['sanger'] = {}
+
+with open('../bernat_et_al/bernat_inferred_sequences.csv', 'r') as fi:
+    reader = csv.reader(fi)
+    for row in reader:
+        if '#' not in row[0] and len(row) > 1:
+            kimdb_accessions['inferred'][row[0].replace(' ', '')] = row[1]
+
+with open('../bernat_et_al/bernat_sanger_sequences.csv', 'r') as fi:
+    reader = csv.reader(fi)
+    for row in reader:
+        if '#' not in row[0] and len(row) > 2 and row[2] == 'R':
+            kimdb_accessions['sanger'][row[0].replace(' ', '')] = row[1]
+
+
+seq_notes = {}
+seq_notes['rhgldb'] = {}
+seq_notes['kimdb'] = {}
+
+used_seq_notes = {}
+used_seq_notes['rhgldb'] = []
+used_seq_notes['kimdb'] = []
+
+for row in simple.read_csv('../sequence_notes.csv'):
+    if 'rhgldb' in row['name']:
+        seq_notes['rhgldb'][row['name'].replace('rhgldb:', '')] = row['notes']
+    elif 'kimdb' in row['name']:
+        seq_notes['kimdb'][row['name'].replace('kimdb:', '')] = row['notes']
     else:
-        print('unexpected name in imgt: %s - record dropped' % name)
-        continue
-    row['Gene Sequence'] = seq
+        print('Unrecognised sequence name in notes: %s' % row)
 
-    if row['Gene Sequence'] not in imgt:
-        imgt[row['Gene Sequence']] = []
-    imgt[row['Gene Sequence']].append(row)
 
+
+def read_imgt():
+    ret = {}
+
+    for qualifier,  filename in imgt_files.items():
+        genes = simple.read_fasta(filename)
+        for name, seq in genes.items():
+            if  seq not in ret:
+                ret[seq] = {}
+
+                if 'IGHD' in name:
+                    ret[seq]['Type'] = 'IGHD'
+                elif 'IGHJ' in name:
+                    ret[seq]['Type'] = 'IGHJ'
+                elif 'IGHV' in name:
+                    ret[seq]['Type'] = 'IGHV'
+                else:
+                    print('unexpected name in imgt file %s: %s - record dropped' % (filename, name))
+                    continue
+                ret[seq]['Gene Sequence'] = seq
+                ret[seq]['Names'] = {}
+
+            if name not in ret[seq]['Names']:
+                ret[seq]['Names'][name] = []
+
+            ret[seq]['Names'][name].append(qualifier)
+
+    for seq, row in ret.items():
+        consolidated_names = []
+        for name, qualifiers in row['Names'].items():
+            if len(qualifiers) == len(imgt_files):
+                consolidated_name = name
+            elif len(row['Names']) == 1 and len(qualifiers) == 1 and qualifiers[0] == 'post Sept 2021':
+                consolidated_name = name
+            else:
+                consolidated_name = name + ' (' + ', '.join(qualifiers) + ')'
+            consolidated_names.append(consolidated_name)
+
+        ret[seq]['Gene Name'] = ', '.join(consolidated_names)
+
+    return ret
+
+imgt = read_imgt()
 
 def add_name_and_type(rec, label, type):
     if rec['type'] is None:
         rec['type'] = type
         rec['gene_label'] = type + '-' + label
-
     return
 
 all_pubids = []
 
-headers = ['gene_label', 'type', 'functional', 'inference_type', 'species_subgroup', 'kimdb', 'rhgldb', 'imgt', 'pubid', 'genbank', 'alt_names', 'notes', 'sequence', 'sequence_gapped']
+headers = ['gene_label', 'type', 'functional', 'inference_type', 'species_subgroup', 'subgroup_type', 'kimdb', 'rhgldb', 'imgt', 'pubid', 'genbank', 'alt_names', 'notes', 'sequence', 'sequence_gapped']
 
 with open('macaca_mulatta_db.csv', 'r') as db, open('rhesus_consolidated.csv', 'w', newline='') as fo:
     reader = csv.DictReader(db)
@@ -89,32 +155,37 @@ with open('macaca_mulatta_db.csv', 'r') as db, open('rhesus_consolidated.csv', '
     notfound = 0
     for row in reader:
         for seq in row['sequences'].split(','):
-            if seq not in kimdb and seq not in rhgldb:
-                notfound += 1
-                continue
             rearranged = False
             unrearranged = False
             alt_names = []
+            notes = []
             rec = {'gene_label': '', 'type': None, 'kimdb': '', 'rhgldb': '', 'imgt': '', 'genbank': '', 'pubid': '',
                    'inference_type': '', 'sequence': seq, 'sequence_gapped': seq, 'notes': '', 'functional': 'Y',
-                   'species_subgroup': '', 'alt_names': ''}
+                   'species_subgroup': '', 'subgroup_type': '', 'alt_names': ''}
+            genbanks = []
             if seq in kimdb:
                 names = []
                 for entry in kimdb[seq]:
                     names.append(entry['Gene Name'])
                     alt_names.append('kimdb:' + entry['Gene Name'])
                     add_name_and_type(rec, row['label'], entry['Type'])
+                    if entry['Gene Name'] in kimdb_accessions['inferred']:
+                        genbanks.append(kimdb_accessions['inferred'][entry['Gene Name']])
+                        rearranged = True
+                    if entry['Gene Name'] in kimdb_accessions['sanger']:
+                        genbanks.append(kimdb_accessions['sanger'][entry['Gene Name']])
+                        unrearranged = True
+                    if entry['Gene Name'] in seq_notes['kimdb']:
+                        notes.append(seq_notes['kimdb'][entry['Gene Name']])
+                        used_seq_notes['kimdb'].append(entry['Gene Name'])
                 rec['kimdb'] = ', '.join(names)
-                rearranged = True
+
             if seq in imgt:
-                names = []
-                for entry in imgt[seq]:
-                    names.append(entry['Gene Name'])
-                    add_name_and_type(rec, row['label'], entry['Type'])
-                rec['imgt'] = ', '.join(names)
+                rec['imgt'] = imgt[seq]['Gene Name']
+                gene_type = imgt[seq]['Gene Name'][:4]
+                add_name_and_type(rec, row['label'], gene_type)
             if seq in rhgldb:
                 names = []
-                genbanks = []
                 pubids = []
                 for entry in rhgldb[seq]:
                     names.append(entry['gene_name'])
@@ -126,33 +197,47 @@ with open('macaca_mulatta_db.csv', 'r') as db, open('rhesus_consolidated.csv', '
                     pubids.extend(entry['pmid'].split(';'))
                     all_pubids.extend(entry['pmid'].split(';'))
                     add_name_and_type(rec, row['label'], entry['gene_type'])
+                    if entry['gene_name'] in seq_notes['rhgldb']:
+                        notes.append(seq_notes['rhgldb'][entry['gene_name']])
+                        used_seq_notes['rhgldb'].append(entry['gene_name'])
 
-                if entry['gene_confidence'] == '1' or entry['gene_confidence'] == '2':
-                    unrearranged = True
-                else:
-                    rearranged = True
-
+                    if entry['gene_confidence'] == '1' or entry['gene_confidence'] == '2':
+                        unrearranged = True
+                    else:
+                        rearranged = True
 
                 rec['rhgldb'] = ', '.join(names)
-                rec['genbank'] = ','.join(genbanks)
                 rec['pubid'] = ','.join(pubids)
             if rec['type'] == 'IGHV':
-                res, aa, notes = number_ighv.gap_sequence(rec['sequence'], imgt_v_gapped, imgt_v_ungapped)
+                res, aa, numb_notes = number_ighv.gap_sequence(rec['sequence'], imgt_v_gapped, imgt_v_ungapped)
                 rec['sequence_gapped'] = res
-                if notes:
-                    rec['notes'] += notes
+                if numb_notes:
+                    notes.append(numb_notes)
+
+            rec['notes'] = ', '.join(notes)
+            rec['genbank'] = ','.join(genbanks)
+
             if rearranged and unrearranged:
                 rec['inference_type'] = 'Both'
-            elif unrearranged:
-                rec['inference_type'] = 'Unrearranged Only'
-            else:
-                rec['inference_type'] = 'Rearranged Only'
+            if rearranged:
+                rec['inference_type'] = 'Rearranged'
+            if unrearranged:
+                rec['inference_type'] = 'Unrearranged'
 
             rec['alt_names'] = ','.join(alt_names)
             writer.writerow(rec)
 
     if notfound > 0:
         print('%d sequences in macaca_mulatta_db.csv were not found in rhesus_consolidated.csv' % notfound)
+
+    for db_name in ['kimdb', 'rhgldb']:
+        notused = []
+        for name in seq_notes[db_name]:
+            if name not in used_seq_notes[db_name]:
+                notused.append(name)
+
+        if len(notused):
+            print('Unused notes for %s: %s' % (db_name, ', '.join(notused)))
 
 #all_pubids = list(set(all_pubids))
 #for pub in all_pubids:
